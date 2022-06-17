18252199d3996c4f704cf117768fb753b5a6b4d1
Gustavo Daniel <gudaniel1@gmail.com>
2022-06-17 09:33:57 -0300

    Adicionada View que obtem a data do evento através do título

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import *
+
 
 # Create your views here.
+def get_event_date_by_title(request, titulo_evento):
+    return HttpResponse(f'{Evento.objects.get(titulo=titulo_evento).data_evento:A data do evento é %x às %X}')
