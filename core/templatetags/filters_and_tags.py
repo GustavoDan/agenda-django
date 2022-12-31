e3be55d2f2637027502ab5ac40e54d985cbf4f77
Gustavo Daniel <gudaniel1@gmail.com>
2022-12-31 07:39:45 -0300

    Criação de filtros e tags customizados.

---
+++ core/templatetags/filters_and_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='split')
+def split(value, separator):
+    return value.split(separator)
+
+@register.simple_tag
+def replace(value, old, new):
+    return value.replace(old, new)
